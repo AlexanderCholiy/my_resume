a1af0f40fe4cdb85ba06eb1a89173ac33fa02af7
Alexander Choliy <alexander.choliy@gmail.com>
2025-03-08 00:24:25 +0300

    deploy on pythonanywhere

--- my_resume/my_resume/settings.py
+++ my_resume/my_resume/settings.py
@@ -100,6 +100,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_ROOT = BASE_DIR / 'media'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.MyUser'
